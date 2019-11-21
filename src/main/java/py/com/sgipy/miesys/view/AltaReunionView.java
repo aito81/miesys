096ac1810eec340f78c3d1ddf1fe58ca34c5de43
Santiago Balbuena <35033376+aito81@users.noreply.github.com>
2019-11-21 16:58:44 -0300

    www
    ssasas

---
+++ src/main/java/py/com/sgipy/miesys/view/AltaReunionView.java
@@ -0,0 +1,18 @@
+package py.com.sgipy.miesys.view;
+
+
+import com.vaadin.navigator.View;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.VerticalLayout;
+
+import py.com.sgipy.miesys.util.ViewConfig;
+
+
+@ViewConfig(uri = "ReunionAlta", displayName = "Alta de Reuniones")
+public class AltaReunionView extends CustomComponent implements View{
+	
+	private VerticalLayout mainLayout;
+	private VerticalLayout formLayout;
+	
+
+}
